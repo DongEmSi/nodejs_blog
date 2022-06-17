89cc67fa40944b03b05872e621fb0dc08130de58
DongEmSi <ledongdong102@gmail.com>
2022-06-17 23:27:33 +0700

    Update 1.1 commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express')
+const { engine } = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+//use static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP Logger => Dễ dàng debug, biết đường dẫn,...
+app.use(morgan('combined'))
+
+//Template engine 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+//app.enable('view cache')
+
+app.get('/', (req, res) => { //arrow function
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`) //http://localhost:3000/ 
+})
